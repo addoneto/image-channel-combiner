2a61fb9162ffa2f8ed063904e28e50afd25e43a8
addo <addo.neto@gmail.com>
2021-02-23 20:07:16 -0300

    Non-Params executable version

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
+using System.IO;
 
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
-using SixLabors.ImageSharp.Processing;
 
 // dotnet add package SixLabors.ImageSharp --version 1.0.3
 // <PackageReference Include="SixLabors.ImageSharp" Version="1.0.3" />
@@ -14,41 +14,69 @@ namespace imageChannelCombiner {
                 GenerateImage(paths[0], paths[1], paths[2]);
             }else {
                 Console.WriteLine("Provide image paths for each channel [R, G, B]");
+                Console.Write("Red Channel Image: ");
+                string readPath  = Console.ReadLine();
+
+                Console.Write("Green Channel Image: ");
+                string greenPath = Console.ReadLine();
+
+                Console.Write("Blue Channel Image: ");
+                string bluePath  = Console.ReadLine();
+
+                Console.Write("Image final name: ");
+                string finalName = Console.ReadLine();
+
+                GenerateImage(readPath, greenPath, bluePath, finalName);
             }
         }
 
-        static void GenerateImage(string rPath, string gPath, string bPath){
+        static void GenerateImage(string rPath, string gPath, string bPath, string finalName = "final"){
+
+            Image<Rgba32> rImg;
+            Image<Rgba32> bImg;
+            Image<Rgba32> gImg;
 
             try {
-                using Image<Rgba32> rImg = Image.Load<Rgba32>(rPath);
-                using Image<Rgba32> bImg = Image.Load<Rgba32>(gPath);
-                using Image<Rgba32> gImg = Image.Load<Rgba32>(bPath);
-
-                if(rImg.Width != bImg.Width || rImg.Width != gImg.Width 
-                    || rImg.Height != bImg.Height || rImg.Height != gImg.Height){
-                    
-                    Console.WriteLine("All images must be the same size");
-                    return;
-                }
-
-                using Image<Rgba32> final = new Image<Rgba32>(rImg.Width, rImg.Height);
-
-                for(int y = 0; y < rImg.Height; y++){
-                    for(int x = 0; x < rImg.Width; x++){
-                        // Could use any channel because images should be gray scl
-                        byte redChannel   = rImg[x, y].R;
-                        byte greenChannel = bImg[x, y].G;
-                        byte blueChannel  = gImg[x, y].B;
-
-                        final[x, y] = new Rgba32(redChannel, greenChannel, blueChannel, 255);
-                    }
-                }
-
-                final.Save("test.png"); 
+                rImg = Image.Load<Rgba32>(rPath);
+                bImg = Image.Load<Rgba32>(gPath);
+                gImg = Image.Load<Rgba32>(bPath);
             }catch(Exception err){
                 Console.WriteLine($"{err.GetType().Name}: Could not load images");
                 return; // throw;
             }
+
+            // IImageFormat imgFormat = rImg.DetectFormat(byte[] data);
+            string rFileExtension = Path.GetExtension(rPath);
+            string gFileExtension = Path.GetExtension(gPath);
+            string bFileExtension = Path.GetExtension(bPath);
+
+            // TODO: try converting file extensions
+            if(rFileExtension != bFileExtension || rFileExtension != gFileExtension){
+                Console.WriteLine("All files must be the same format");
+                return;
+            }
+
+            if(rImg.Width != bImg.Width || rImg.Width != gImg.Width 
+                || rImg.Height != bImg.Height || rImg.Height != gImg.Height){
+                    
+                Console.WriteLine("All images must be the same size");
+                return;
+            }
+
+            using Image<Rgba32> final = new Image<Rgba32>(rImg.Width, rImg.Height);
+
+            for(int y = 0; y < rImg.Height; y++){
+                for(int x = 0; x < rImg.Width; x++){
+                    // Could use any channel because images should be gray scl
+                    byte redChannel   = rImg[x, y].R;
+                    byte greenChannel = bImg[x, y].G;
+                    byte blueChannel  = gImg[x, y].B;
+
+                    final[x, y] = new Rgba32(redChannel, greenChannel, blueChannel, 255);
+                }
+            }
+
+            final.Save(finalName + rFileExtension); 
         }
     }
 }
--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 ## Results
 
-![resul](result.png)
-![resul1](result1.png)
+| Red | Green | Blue |
+|------------|-----------|----------|
+|![red](media/mars-red.png) |  ![green](media/mars-green.png)  |![blue](media/mars-blue.png)|
+
+![resul](media/result.png)
 
 [From](https://mars.nasa.gov/mars2020/multimedia/raw-images/)
